9e8bee9ad4af717e4069941aba19c039d11392fa
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-12 17:50:46 -0400

    small fix

--- packages/nouns-webapp/src/components/Documentation/index.tsx
+++ packages/nouns-webapp/src/components/Documentation/index.tsx
@@ -22,10 +22,8 @@ The Navigators of the ZK Cypherpunk Zeitgeist are integral to the work of solvin
         <div className={classes.headerWrapper}>
           <h1>How it Works?</h1>
           <p className={classes.aboutText}>
-          At varying intervals NFTs are placed live for auction on Ethereum with a reserve price and associated cypher. Anyone can bid publicly on the NFT within a 24 hour period. The highest bidder receives the NFT at the end of the auction. If they also put in the work to solve the cypher they unlock the F₃M governance token bounty. To receive the governance bounty you must meet the reserve, win the auction and solve the cypher. If you don't solve the cypher you only receive the NFT.
-            <br />
-            <br />
-            If a reserve is not met within the 24 hour period of the NFT going live then it is moved to the private auction on Polygon. Here, only F₃M governance token holders are able to bid on the piece. They must also solve the cypher in order to receive the bounty along with the content during the private auction.
+NFTs are placed live for auction daily on Ethereum with a reserve price and associated cypher. Anyone can bid publicly on the NFT within a 24 hour period. The highest bidder receives the NFT at the end of the auction. If they also put in the work to solve the cypher they unlock the F₃M governance token bounty. To receive the governance bounty you must meet the reserve, win the auction and solve the cypher. If you don't solve the cypher you only receive the NFT. <br><\br><br><\br>
+If a reserve is not met within the 24 hour period of the NFT going live then it is moved to the private auction on Polygon. Here, only F₃M governance token holders are able to bid on the piece over the 24 hour period. They must also solve the cypher in order to receive the bounty along with the content during the private auction. 
           </p>
         </div>
 
